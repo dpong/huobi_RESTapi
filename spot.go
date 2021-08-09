c03a66498add11a9dcd033a30d10a945e87a67b1
dpong <powenperng@gmail.com>
2021-08-09 17:08:24 +0800

    Updated.

--- spot.go
+++ spot.go
@@ -6,6 +6,48 @@ import (
 	"strconv"
 )
 
+type SpotsResponse struct {
+	Status string `json:"status"`
+	Data   []struct {
+		BaseCurrency             string  `json:"base-currency"`
+		QuoteCurrency            string  `json:"quote-currency"`
+		PricePrecision           int     `json:"price-precision"`
+		AmountPrecision          int     `json:"amount-precision"`
+		SymbolPartition          string  `json:"symbol-partition,omitempty"`
+		Symbol                   string  `json:"symbol"`
+		State                    string  `json:"state"`
+		ValuePrecision           int     `json:"value-precision"`
+		MinOrderAmt              float64 `json:"min-order-amt"`
+		MaxOrderAmt              float64 `json:"max-order-amt,omitempty"`
+		MinOrderValue            float64 `json:"min-order-value"`
+		LimitOrderMinOrderAmt    float64 `json:"limit-order-min-order-amt,omitempty"`
+		LimitOrderMaxOrderAmt    float64 `json:"limit-order-max-order-amt,omitempty"`
+		SellMarketMinOrderAmt    float64 `json:"sell-market-min-order-amt,omitempty"`
+		SellMarketMaxOrderAmt    float64 `json:"sell-market-max-order-amt,omitempty"`
+		BuyMarketMaxOrderValue   float64 `json:"buy-market-max-order-value,omitempty"`
+		LeverageRatio            float64 `json:"leverage-ratio,omitempty"`
+		SuperMarginLeverageRatio float64 `json:"super-margin-leverage-ratio,omitempty"`
+		FundingLeverageRatio     float64 `json:"funding-leverage-ratio,omitempty"`
+		APITrading               string  `json:"api-trading"`
+		Tags                     string  `json:"tags,omitempty"`
+	} `json:"data"`
+}
+
+func (p *Client) Spots() (swaps *SpotsResponse, err error) {
+	res, err := p.sendRequest("spot", http.MethodGet, "/v1/common/symbols", nil, nil, false)
+	if err != nil {
+		p.Logger.Println(err)
+		return nil, err
+	}
+	// in Close()
+	err = decode(res, &swaps)
+	if err != nil {
+		p.Logger.Println(err)
+		return nil, err
+	}
+	return swaps, nil
+}
+
 type GetAllAccountsResponse struct {
 	Data []struct {
 		ID      int    `json:"id"`
@@ -33,9 +75,9 @@ func (p *Client) GetAllAccounts() (swaps *GetAllAccountsResponse, err error) {
 type GetAccountDataResponse struct {
 	Status string `json:"status"`
 	Data   struct {
-		ID    int            `json:"id"`
-		Type  string         `json:"type"`
-		State string         `json:"state"`
+		ID    int             `json:"id"`
+		Type  string          `json:"type"`
+		State string          `json:"state"`
 		List  []*SpotListData `json:"list"`
 	} `json:"data"`
 }
