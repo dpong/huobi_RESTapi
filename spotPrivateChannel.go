c3633462cc4677eee31c470d92371431e7e69b29
assc_powen <assc_powen@assc-powendeMacBook-Pro.local>
2022-09-16 11:53:54 +0800

    updated. block account websocket updating.

--- spotPrivateChannel.go
+++ spotPrivateChannel.go
@@ -223,31 +223,31 @@ func (u *spotPrivateChannelBranch) maintainSpotUserData(
 				continue
 			}
 			switch {
-			// case strings.Contains(ch, "accounts.update"):
-			// 	data, ok := message["data"].(map[string]interface{})
-			// 	if !ok {
-			// 		continue
-			// 	}
-			// 	id, ok := data["accountId"].(float64)
-			// 	if !ok {
-			// 		continue
-			// 	}
-			// 	if id != float64(u.accountID) {
-			// 		continue
-			// 	}
-			// 	accountType, ok := data["accountType"].(string)
-			// 	if !ok {
-			// 		continue
-			// 	}
-			// 	balance, ok := data["balance"].(string)
-			// 	if !ok {
-			// 		continue
-			// 	}
-			// 	currency, ok := data["currency"].(string)
-			// 	if !ok {
-			// 		continue
-			// 	}
-			// 	u.updateSpotAccountData(currency, accountType, balance)
+			case strings.Contains(ch, "accounts.update"):
+				data, ok := message["data"].(map[string]interface{})
+				if !ok {
+					continue
+				}
+				id, ok := data["accountId"].(float64)
+				if !ok {
+					continue
+				}
+				if id != float64(u.accountID) {
+					continue
+				}
+				accountType, ok := data["accountType"].(string)
+				if !ok {
+					continue
+				}
+				balance, ok := data["balance"].(string)
+				if !ok {
+					continue
+				}
+				currency, ok := data["currency"].(string)
+				if !ok {
+					continue
+				}
+				u.updateSpotAccountData(currency, accountType, balance)
 			case strings.Contains(ch, "trade.clearing"):
 				if event, ok := message["eventType"].(string); ok {
 					if event != "trade" {
@@ -525,9 +525,9 @@ func (w *wS) HandleHuobiSpotUserData(res *map[string]interface{}, mainCh *chan m
 			}
 			logger.Infof("Huobi user data auth success")
 			// sub account update
-			if err := w.subscribeAccountUpdateEvent(); err != nil {
-				return err
-			}
+			// if err := w.subscribeAccountUpdateEvent(); err != nil {
+			// 	return err
+			// }
 			if err := w.batchSubscribeTradeEvent(w.symbols); err != nil {
 				return err
 			}
